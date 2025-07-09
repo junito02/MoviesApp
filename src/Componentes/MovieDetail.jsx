72df75af60a8da96ec1edc917de7fe0f20d19cc5
Junior Alejandro <junioralejdandrotiburcio@gmail.com>
2025-07-08 22:34:47 -0400

    fixing-bug

--- src/Componentes/MovieDetail.jsx
+++ src/Componentes/MovieDetail.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import Credits from "./Credits";
 
 const MovieDetail = () => {
   const { id } = useParams();
